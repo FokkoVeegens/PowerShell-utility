656bc573ff28c8563b1cc7fb5f234439d722c2be
Fokko <24793348+FokkoVeegens@users.noreply.github.com>
2021-08-31 14:37:11 +0200

    Bugfixes

--- export-all-workitemtype.ps1
+++ export-all-workitemtype.ps1
@@ -1,4 +1,6 @@
 # Currently only useable for on-prem installations (Azure DevOps Server)
+# Retrieves max 2000 Team Projects
+
 $coll = "https://servername:8080/tfs/defaultcollection"
 $apiurl = "$coll/_apis"
 $witadminpath = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\CommonExtensions\Microsoft\TeamFoundation\Team Explorer\witadmin.exe"
@@ -30,9 +32,11 @@ function Export-Wit($teamproject, $witname, $basepath)
     witadmin exportwitd /collection:$coll /p:$teamproject /n:$witname /f:$targetfile
 }
 
-$uri = "$apiurl/projects"
-$projects = (invoke-webrequest -Uri $uri -Method GET -ContentType "application/json").value | ConvertFrom-Json
+$uri = "$apiurl/projects?`$top=2000"
+$response = invoke-webrequest -Uri $uri -Method GET -ContentType "application/json" -UseDefaultCredentials
+$projects = ($response.Content | ConvertFrom-Json).value
 
+Write-Host "Number of projects found: $($projects.Count)."
 foreach ($project in $projects)
 {
     $witdlist = List-Wits -teamproject $project.name
